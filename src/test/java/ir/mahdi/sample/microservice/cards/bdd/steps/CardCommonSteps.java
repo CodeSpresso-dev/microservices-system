3bd8d1ba02bf6950c93e887598244ef4660e851b
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-01 02:33:31 +0330

    refactor: move Given condition to common step due to using in multiple scenarios

--- src/test/java/ir/mahdi/sample/microservice/cards/bdd/steps/CardCommonSteps.java
+++ src/test/java/ir/mahdi/sample/microservice/cards/bdd/steps/CardCommonSteps.java
@@ -1,8 +1,10 @@
 package ir.mahdi.sample.microservice.cards.bdd.steps;
 
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import ir.mahdi.sample.microservice.cards.bdd.context.CardScenarioContext;
+import ir.mahdi.sample.microservice.cards.reository.CardRepository;
 import ir.mahdi.sample.microservice.cards.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,6 +21,9 @@ public class CardCommonSteps {
     @Autowired
     private CardService cardService;
 
+    @Autowired
+    private CardRepository cardRepository;
+
     @When("I request to create a card")
     public void create_card() {
 
@@ -27,6 +32,12 @@ public class CardCommonSteps {
         } catch (Exception ex) {
             context.setException(ex);
         }
+    }
+
+    @Given("card with id {long} does not exist")
+    public void card_with_id_does_not_exist(Long id) {
+
+        cardRepository.deleteById(id);
     }
 
     @Then("{string} should be thrown")
--- src/test/java/ir/mahdi/sample/microservice/cards/bdd/steps/DeleteNonExistingCard.java
+++ src/test/java/ir/mahdi/sample/microservice/cards/bdd/steps/DeleteNonExistingCard.java
@@ -12,19 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class DeleteNonExistingCard {
 
     @Autowired
-    private CardRepository cardRepository;
-
-    @Autowired
     private CardService cardService;
 
     @Autowired
     private CardScenarioContext context;
-
-    @Given("card with id {long} does not exist")
-    public void card_does_not_exist(Long id) {
-
-        cardRepository.deleteAll();
-    }
 
     @When("I delete the card with id {long}")
     public void delete_card_with_id(Long id) {
