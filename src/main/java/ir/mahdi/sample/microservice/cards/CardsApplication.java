68d8e6619c190b00bf87022d1e3b43c4e1a306de
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-05-31 12:41:51 +0330

    feat: implement audit ability and enable it

--- src/main/java/ir/mahdi/sample/microservice/cards/CardsApplication.java
+++ src/main/java/ir/mahdi/sample/microservice/cards/CardsApplication.java
@@ -2,8 +2,10 @@ package ir.mahdi.sample.microservice.cards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class CardsApplication {
 
     public static void main(String[] args) {
