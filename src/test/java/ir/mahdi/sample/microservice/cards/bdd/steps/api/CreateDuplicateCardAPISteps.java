1130d89d8238e6253640b3db27d5716bd1c7a19c
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-01 11:55:51 +0330

    feat: implement Scenario: Create duplicate card through API | BDD SIDE

---
+++ src/test/java/ir/mahdi/sample/microservice/cards/bdd/steps/api/CreateDuplicateCardAPISteps.java
@@ -0,0 +1,42 @@
+package ir.mahdi.sample.microservice.cards.bdd.steps.api;
+
+import io.cucumber.java.en.Given;
+import ir.mahdi.sample.microservice.cards.bdd.builder.CardBuilder;
+import ir.mahdi.sample.microservice.cards.bdd.builder.CreateCardRequestBuilder;
+import ir.mahdi.sample.microservice.cards.bdd.context.CardScenarioContext;
+import ir.mahdi.sample.microservice.cards.dto.request.CreateCardRequest;
+import ir.mahdi.sample.microservice.cards.entity.Card;
+import ir.mahdi.sample.microservice.cards.reository.CardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CreateDuplicateCardAPISteps {
+
+    @Autowired
+    private CardScenarioContext context;
+
+    @Autowired
+    private CardRepository cardRepository;
+
+    private String cardNumber;
+
+    @Given("a card with card number {string} already exists")
+    public void card_with_card_number_already_exists(String cardNumber) {
+        this.cardNumber = cardNumber;
+        Card card = CardBuilder.DefaultCard();
+        card.setCardNumber(cardNumber);
+
+        cardRepository.save(card);
+    }
+
+    @Given("a valid card creation request with existence card number")
+    public void valid_card_creation_request_with_existence_card_number() {
+
+        CreateCardRequest req = CreateCardRequestBuilder.valid();
+        req.setCardNumber(this.cardNumber);
+
+        context.setRequest(req);
+    }
+
+}
--- src/test/resources/features/api/create-card-api.feature
+++ src/test/resources/features/api/create-card-api.feature
@@ -8,3 +8,13 @@ Feature: Card API
     Then api response status should be 201
 
     And api response should contain cardId
+
+  Scenario: Create duplicate card through API
+
+    Given a card with card number "1234567892548962" already exists
+
+    And a valid card creation request with existence card number
+
+    When I send POST request to "/api/cards"
+
+    Then api response status should be 409
